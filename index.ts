fe36214f8e90d25935d85cb87f4ebc9e98476d26
jonclawson <30538332+jonclawson@users.noreply.github.com>
2023-08-02 23:46:36 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -120,7 +120,7 @@ function run() {
     <div class="accordion-item">
       <h2 class="accordion-header">
         <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Accordion Item #1
+          Show Log
         </button>
       </h2>
       <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
